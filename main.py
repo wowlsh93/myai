9c70545bd363752c55ecaec41a750de4d2140523
hama <wowlsh93@gmail.com>
2023-11-17 17:13:01 +0900

    add init file

--- main.py
+++ main.py
@@ -1,31 +1,47 @@
 import os
-# from dotenv import load_dotenv
-# load_dotenv()
+from dotenv import load_dotenv
+
 
 #AI
-from langchain.llms import OpenAI
+from langchain.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
 from langchain.chat_models import ChatOpenAI
-
+from langchain.retrievers.multi_query import MultiQueryRetriever
+from langchain.chains import RetrievalQA
 
 #UI
 import streamlit as st
 
 
 if __name__ == '__main__':
+    load_dotenv()
+    loader = PyPDFLoader("unsu.pdf")
+    pages = loader.load_and_split()
 
-    openai_key = os.getenv("OPENAI_API_KEY")
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=300,
+        chunk_overlap=20,
+        length_function=len,
+        is_separator_regex=False,
+    )
 
-    chat_model = ChatOpenAI()
+    texts = text_splitter.split_documents(pages)
 
-    st.title("AI Poet")
+    embeddings_model = OpenAIEmbeddings()
 
-    content = st.text_input("Please suggest subject of Poem What you want", "")
+    db = Chroma.from_documents(texts, embeddings_model)
 
 
-    if st.button("Answer"):
-        with st.spinner("wait for it .."):
-            result2 = chat_model.predict("please poet to me about " + content)
-            st.write(result2)
+    #question!!
+    question = "내용을 요약해줘"
+    llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+    qa_chain = RetrievalQA.from_chain_type(llm, retriever=db.as_retriever())
+    result = qa_chain({"query": question})
+    print(result)
+
+
 
 
 
