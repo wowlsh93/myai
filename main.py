ea2289c1696fdc674f7ef237c3065388c315c94e
hama <wowlsh93@gmail.com>
2023-11-24 15:08:46 +0900

    change code

--- main.py
+++ main.py
@@ -18,16 +18,12 @@ from langchain.chains import RetrievalQA
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 import tempfile
 import os
+from PIL import Image
+
 
 load_dotenv()
 
-st.header('PDF Genie')
-st.write('질문하실 PDF를 업로드해주세요.')
-st.write('---')
-
-
-# # Make a temp folder can store uploaded file.
-
+#Make a temp folder can store uploaded file.
 def pdf_to_document(uploaded_file):
     temp_dir = tempfile.TemporaryDirectory()
     temp_filepath = os.path.join(temp_dir.name, uploaded_file.name)
@@ -50,15 +46,18 @@ class StreamingHandler(BaseCallbackHandler):
         self.text += token
         self.container.markdown(self.text)
 
+if __name__ == "__main__":
 
-with st.sidebar:
-    st.header('파일업로드')
+    image = Image.open('tt.png')
+
+    st.image(image, width=200)
+
+    st.header('Chat with PDF!!')
+
     uploaded_files = st.file_uploader(
-        '파일을 선택해주세요.',
+        'PDF를 업로드해주세요.',
         accept_multiple_files=True
     )
-
-    # uploaded_file_no = (uploaded_file)
     if uploaded_files:
         # load text
         for uploaded_file in uploaded_files:
@@ -78,30 +77,33 @@ with st.sidebar:
             embedding_model = OpenAIEmbeddings()
             db = Chroma.from_documents(texts, embedding_model)
 
-            st.write(bytes_data.name, 'File DONE')
+            st.write(bytes_data.name, ' is uploaded!!')
 
-# show input box
-question = st.text_input('질문을 입력해주세요.')
-if st.button('궁금해', type="primary"):
-    with st.spinner('처리중입니다'):
-        chat_box = st.empty()
-        stream_hander = StreamingHandler(chat_box)
-        myllm = ChatOpenAI(
-            temperature=0,
-            max_tokens=100,
-            streaming=True,
-            callbacks=[stream_hander],
-        )
-        qa = RetrievalQA.from_chain_type(
-            llm=myllm,
-            retriever=db.as_retriever()
-        )
-        answer = qa.run(question)
-        qa.run(question)
-        # st.write('질문 :', question)
-        # st.write('답변 :', answer)
+    st.write('---')
+    # show input box
 
-print('DONE')
+    question = st.text_input('질문을 입력해주세요.')
+    if st.button('궁금해', type="primary"):
+        with st.spinner('처리중입니다'):
+            chat_box = st.empty()
+            stream_hander = StreamingHandler(chat_box)
+            myllm = ChatOpenAI(
+                temperature=0,
+                max_tokens=100,
+                streaming=True,
+                callbacks=[stream_hander],
+            )
+            qa = RetrievalQA.from_chain_type(
+                llm=myllm,
+                retriever=db.as_retriever()
+            )
+            answer = qa.run(question)
+            qa.run(question)
+
+    print('DONE')
+
+
+
 
 
 
